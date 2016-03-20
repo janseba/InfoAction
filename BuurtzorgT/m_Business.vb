3d1cbb7cc74657e4cae99ae9438ee70d9a405a18
janseba <bas.jansen@infoaction.nl>
2016-03-20 14:57:51 +0100

    Code om te importeren verder uitgebreid (vooral databaselaag toegevoegd.

--- BuurtzorgT/m_Business.vb
+++ BuurtzorgT/m_Business.vb
@@ -1,8 +1,30 @@
 Option Explicit
 
 Sub getCareweb()
-    Dim FilePath As String
-    FilePath = getFileName("Selecteer csv bestand uit Careweb")
+    Dim carewebCsv As String
+    carewebCsv = getFileName("Selecteer csv bestand uit Careweb")
+    FileCopy carewebCsv, gsDatabasePath & "import.csv"
+    
+End Sub
+Sub importCSV()
+    Dim csvConnnection As ADODB.Connection, csvRecords As ADODB.Recordset, dbRecords As ADODB.Recordset, f As Variant
+    Set csvConnnection = New ADODB.Connection
+    Set csvRecords = New ADODB.Recordset
+    csvConnnection.Provider = "Microsoft.Jet.OLEDB.4.0"
+    csvConnnection.ConnectionString = "Data Source=" & gsDatabasePath & ";" & _
+        "Extended Properties=""text;HDR=Yes;FMT=Delimited;"""
+    csvConnnection.Open
+    csvRecords.Open "SELECT [Hoofdbehandelaar diagnosefase] AS PID FROM [" & gsCSV_FILE & "]", csvConnnection
+    csvRecords.MoveFirst
+    Set dbRecords = GetRecordSet("SELECT * FROM tblTest WHERE DBCNo IS NULL")
+    Do Until csvRecords.EOF
+        dbRecords.AddNew
+        For Each f In csvRecords.Fields
+            dbRecords.Fields(f.Name) = f.Value
+        Next f
+        csvRecords.MoveNext
+    Loop
+    UpdateRecords dbRecords
 End Sub
 
 Sub maintainSettings()
---
+++ BuurtzorgT/m_Database.vb
@@ -0,0 +1,46 @@
+Option Explicit
+Private mcnConnection As ADODB.Connection
+Function GetRecordSet(ByRef sSQL As String) As ADODB.Recordset
+    Dim rsData As ADODB.Recordset
+    
+    If mcnConnection Is Nothing Then bCreateDBConnection gsDatabasePath & gsDATABASE_FILE
+    mcnConnection.Open
+    
+    Set rsData = New ADODB.Recordset
+    With rsData
+        .CursorLocation = adUseClient
+        .CursorType = adOpenStatic
+        .LockType = adLockBatchOptimistic
+        .Open sSQL, mcnConnection, , , adCmdText
+    End With
+    Set rsData.ActiveConnection = Nothing
+    mcnConnection.Close
+    Set GetRecordSet = rsData
+End Function
+Sub UpdateRecords(ByRef rsData As ADODB.Recordset)
+    If mcnConnection Is Nothing Then bCreateDBConnection gsDatabasePath & gsDATABASE_FILE
+    mcnConnection.Open
+    rsData.ActiveConnection = mcnConnection
+    rsData.UpdateBatch
+    Set rsData.ActiveConnection = Nothing
+    mcnConnection.Close
+End Sub
+Public Function bCreateDBConnection(ByRef sFullName As String) As Boolean
+    Dim bReturn As Boolean, sConnect As String
+    
+    On Error GoTo ErrorHandler
+    bReturn = True
+    sConnect = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & sFullName & ";"
+    Set mcnConnection = New ADODB.Connection
+    mcnConnection.ConnectionString = sConnect
+    mcnConnection.Open
+    mcnConnection.Close
+    
+ErrorExit:
+    bCreateDBConnection = bReturn
+    Exit Function
+    
+ErrorHandler:
+    bReturn = False
+    Resume ErrorExit
+End Function
